3897d160a6b82262f67b5e3cf8dd26f56736885a
Hamdi LAADHARI <killerwolf06@gmail.com>
2015-05-04 16:20:47 +0200

    s3 bucket is now an env variable

--- backmydataup/backmydataup.sh
+++ backmydataup/backmydataup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 tar czvf /backup/data_`date +%Y%m%d_%H%M%S`.tar /data
-aws s3 sync /backup s3://ndsbackup --endpoint-url=http://fr-1.storage.online.net
+aws s3 sync /backup s3://$S3_BUCKET --endpoint-url=http://fr-1.storage.online.net
