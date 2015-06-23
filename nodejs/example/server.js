59d00be6ae6b5eeb4877401137bb5928d42ac166
Hamdi LAADHARI <killerwolf06@gmail.com>
2015-06-23 22:01:38 +0200

    remove example

--- nodejs/example/server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Salut tout le monde !');
-});
-server.listen(8080,'0.0.0.0',function () {
-  console.log('Listening on http://0.0.0.0:8080');
-});
